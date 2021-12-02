399e7fdc3274a4d51b84159a743bd1eee1e59c3a
Stephen Thompson <s31@umbc.edu>
2021-12-02 02:02:59 -0600

    creating app

---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Hello() {
+  const message = 'Hello world!';
+
+  return (
+    <div className="container">
+      <h2>{message}</h2>
+    </div>
+  );
+}
+
+export default Hello;
