ff54ff20116809c89aef11c0ca5b56d49cd587b4
Stephen Thompson <s31@umbc.edu>
2021-12-02 23:58:51 -0600

    created header

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+    return (
+        <section className="hero is-success">
+            <div className="hero-body">
+                <p class="title">
+                    Stephen Thompson
+                </p>
+                <p class="subtitle">
+                    Mathematician and Web Developer
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default Header;
