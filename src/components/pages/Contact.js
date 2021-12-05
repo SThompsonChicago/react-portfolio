ee70acd5d78fd24295982825d486685068cdf4f9
Stephen Thompson <s31@umbc.edu>
2021-12-05 11:04:57 -0600

    improved contact info

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -13,7 +13,11 @@ export default function Contact() {
 
 
                 <div className="content">
-                    Please <a href="mailto:s31@umbc.edu">email</a> me. 
+                    To get in touch, please <a href="mailto:s31@umbc.edu">email</a> me. 
+                    </div>
+
+                    <div className="content">
+                    You can also connect with me on <a href="https://github.com/SThompsonChicago">GitHub</a>, <a href="https://www.linkedin.com/in/stephen-thompson-77511174/">LinkedIn</a> or <a href="https://www.researchgate.net/profile/Stephen-Thompson-15">ResearchGate</a>.
                     </div>
             </div>
             
